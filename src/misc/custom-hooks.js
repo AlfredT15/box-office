ec3aa63eb63c8b9b166a1c8bd7f1e2d036372007
Alfred Timperley <atimperley15@gmail.com>
2022-01-10 16:13:47 -0500

    Created two more new hooks: useLastQuery and useShow

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,5 +1,6 @@
-import { useEffect, useReducer } from 'react';
-import { ADD, REMOVE } from './constants';
+import { useEffect, useReducer, useState } from 'react';
+import { apiGet } from './config';
+import { ADD, FETCH_FAILED, FETCH_SUCCESS, REMOVE } from './constants';
 
 function usePersistedReducer(reducer, initialState, key) {
   const [state, dispatch] = useReducer(reducer, initialState, initial => {
@@ -28,4 +29,62 @@ function showsReducer(prevState, action) {
 
 export function useShows(key = 'shows') {
   return usePersistedReducer(showsReducer, [], key);
+}
+
+export function useLastQuery(key = 'lastQuery') {
+  const [input, setInput] = useState(() => {
+    const persisted = sessionStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : '';
+  });
+
+  const setPersistedInput = newState => {
+    setInput(newState);
+    sessionStorage.setItem(key, JSON.stringify(newState));
+  };
+
+  return [input, setPersistedInput];
+}
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case FETCH_SUCCESS: {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case FETCH_FAILED: {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+
+export function useShow(showId) {
+  const [state, dispatch] = useReducer(reducer, {
+    show: null,
+    isLoading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          dispatch({ type: FETCH_SUCCESS, show: results });
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: FETCH_FAILED, error: err.message });
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [showId]);
+
+  return state;
 }
